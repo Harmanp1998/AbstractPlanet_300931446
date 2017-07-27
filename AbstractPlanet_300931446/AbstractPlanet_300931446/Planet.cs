e03249a5cece3324a8bea5aea23da5397e7c143f
Harmanpreet Singh <Harmanpreet.preetsingh@gmail.com>
2017-07-27 18:39:39 -0400

    Created abstract Planet Class

---
+++ AbstractPlanet_300931446/AbstractPlanet_300931446/Planet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Harmanpreet Singh
+ * Date: July 27, 2017
+ * Description: This is the abstract class Planet which other subclasses will
+ * derive from.
+ * Version: 0.1 - Class Created
+ */
+namespace AbstractPlanet_300931446
+{
+    /// <summary>
+    /// This is the abstract Class
+    /// </summary>
+    abstract class Planet
+    {
+        //Private Fields(Instance Variables)
+        
+
+        //Public Properties
+        
+        //Constructors
+
+        //Public Methods
+    }
+}
