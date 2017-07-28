37a65808b71932999b5e19189c507d9f449b813b
Harmanpreet Singh <Harmanpreet.preetsingh@gmail.com>
2017-07-28 01:09:25 -0400

    Added IHasMoons Interface

---
+++ AbstractPlanet_300931446/AbstractPlanet_300931446/IHasMoons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPlanet_300931446
+{
+    interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
