0641d657ae48ddbc8bc79e6138d11bc0cef6fe38
Harmanpreet Singh <Harmanpreet.preetsingh@gmail.com>
2017-07-28 01:20:45 -0400

    Added IHabitable Interface

---
+++ AbstractPlanet_300931446/AbstractPlanet_300931446/IHabitable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Harmanpreet Singh
+ * Date: July 27, 2017
+ * Description: This is an Interface that defines a method header Habitable of bool return type.
+ * Version: 0.1 - Created IHabitable interface
+ */
+namespace AbstractPlanet_300931446
+{
+    /// <summary>
+    /// This is the IHabitable interface
+    /// </summary>
+    interface IHabitable
+    {
+        bool Habitable();
+    }
+}
